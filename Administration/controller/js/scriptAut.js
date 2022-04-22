6e2b8cd574c598f1a18efcfc7afcd9ed85434ede
Jonathan BESSE <bessejon@SUPAGRO.INRA.LAN>
2022-04-22 15:38:23 +0200

    Formulaire bientot prêt, ALAIN ME VOILA

--- Administration/controller/js/scriptAut.js
+++ Administration/controller/js/scriptAut.js
@@ -1,13 +1,12 @@
 const aut = document.getElementById('authd');
 
-
+let tab = getAllData();
 function getAllData() {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'index.php?controller=nomenclature_espece&action=autocompleteAut', true);
     xhr.onload = function() {
         if (this.status === 200) {
             const response = JSON.parse(this.responseText);
-            console.log(response);
             tab = response;
             console.log(tab);
         }

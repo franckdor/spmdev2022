4bdd57ce8c6b35bd45cc24aae6ae00ed1af5a7aa
Jonathan-Besse <jonathan.besse@etu.umontpellier.fr>
2022-05-23 11:26:12 +0200

    LibRis library

---
+++ Administration/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '383e9129504871c2044df992d862191c1afe4e49',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '383e9129504871c2044df992d862191c1afe4e49',
+            'dev_requirement' => false,
+        ),
+        'technosophos/libris' => array(
+            'pretty_version' => '2.0.2',
+            'version' => '2.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../technosophos/libris',
+            'aliases' => array(),
+            'reference' => '5b6628ece0e57e90bd54f3b2d8de922dc4cc662c',
+            'dev_requirement' => false,
+        ),
+    ),
+);

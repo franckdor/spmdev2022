e9dce5a1746c5ecac44ccf22927fe9d33e12ccb9
franck dorkeld <franck.dorkeld@inrae.fr>
2022-07-17 16:06:53 +0200

    change index.php

--- Administration/index.php
+++ Administration/index.php
@@ -4,6 +4,7 @@ $DS = DIRECTORY_SEPARATOR;
 $ROOT_FOLDER = __DIR__ ;
 require_once $ROOT_FOLDER . $DS . 'lib' . $DS . 'File.php';
 require_once File::build_path(array('controller','routeur.php'));
-require_once File::build_path(array("lib", "security.php"));
+//require_once File::build_path(array("lib", "security.php"));
+// bugfix fd : 17 juill 2022
+require_once File::build_path(array("lib", "Security.php"));
 ?>
-
